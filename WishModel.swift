49a80e8727a4a1455c0baeb35c985f2278838a02
Swantan Barua <work.swantan@gmail.com>
2025-09-01 21:48:59 +0530

    Add Wish model with title property
    Introduces a Wish class annotated with @Model, containing a title property and initializer. This sets up the basic data structure for wish items.

--- WishModel.swift
+++ WishModel.swift
@@ -5,4 +5,17 @@
 //  Created by Swantan Barua on 01/09/25.
 //
 
+import Foundation
 import SwiftData
+
+@Model
+class Wish {
+    
+    // MARK: - PROPERTIES
+    var title: String
+    
+    // MARK: - INITIALIZER
+    init(title: String) {
+        self.title = title
+    }
+}
