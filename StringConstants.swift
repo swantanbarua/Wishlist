c40ba79327baead91eb150b77a4c487c8c1e3b5f
Swantan Barua <work.swantan@gmail.com>
2025-09-02 00:30:23 +0530

    Add wishlist navigation title and update UI
    Replaced the ContentView's VStack with a NavigationStack and List to display wishes. Added StringConstants.swift to centralize navigation title constants.

---
+++ StringConstants.swift
@@ -0,0 +1,13 @@
+//
+//  StringConstants.swift
+//  Wishlist
+//
+//  Created by Swantan Barua on 02/09/25.
+//
+
+import Foundation
+
+struct NavigationTitle {
+    
+    static let wishlist = "Wishlist"
+}
