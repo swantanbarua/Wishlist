0e1a28654b8f1b749c645f01753d948d95e24863
Swantan Barua <work.swantan@gmail.com>
2025-09-14 21:06:23 +0530

    Initial Commit

---
+++ WishlistTests/WishlistTests.swift
@@ -0,0 +1,17 @@
+//
+//  WishlistTests.swift
+//  WishlistTests
+//
+//  Created by Swantan Barua on 14/09/25.
+//
+
+import Testing
+@testable import Wishlist
+
+struct WishlistTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
