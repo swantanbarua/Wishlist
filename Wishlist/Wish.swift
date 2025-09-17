7ba5d9eaf269ced39e95e053da9dc49bfb0b57ac
Swantan Barua <work.swantan@gmail.com>
2025-09-17 13:47:19 +0530

    Create Wish model

---
+++ Wishlist/Wish.swift
@@ -0,0 +1,20 @@
+//
+//  Wish.swift
+//  Wishlist
+//
+//  Created by Swantan Barua on 17/09/25.
+//
+
+import Foundation
+import SwiftData
+
+struct Wish {
+    
+    // MARK: - PROPERTIES
+    var title: String
+    
+    // MARK: - INIT
+    init(title: String) {
+        self.title = title
+    }
+}
