0d7137f4e27cc4fca6635f49c016f1f452c45de2
Swantan Barua <work.swantan@gmail.com>
2025-11-14 00:04:53 +0530

    Add Wish model for Wishlist feature
    Introduces the Wish class as a SwiftData model with a title property and initializer. This model will be used to represent individual wishes in the Wishlist module.

---
+++ Wishlist/WishModel.swift
@@ -0,0 +1,21 @@
+//
+//  WishModel.swift
+//  Wishlist
+//
+//  Created by Swantan Barua on 14/11/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Wish {
+    
+    // MARK: - PROPERTIES
+    var title: String
+    
+    // MARK: - INITIALIZATION
+    init(title: String) {
+        self.title = title
+    }
+}
