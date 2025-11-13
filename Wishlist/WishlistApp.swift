ab6294d8aa9c6036454cb8e6ac50789d725e5993
Swantan Barua <work.swantan@gmail.com>
2025-11-13 23:54:19 +0530

    Initial commit

---
+++ Wishlist/WishlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishlistApp.swift
+//  Wishlist
+//
+//  Created by Swantan Barua on 13/11/25.
+//
+
+import SwiftUI
+
+@main
+struct WishlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
